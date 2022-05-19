6df5eea3672159cb865f833f591cd38dfab6b436
crocket77 <ethanpfeiffer345@outlook.com>
2022-05-18 21:04:57 -0500

    updating main branch, downloaded react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,12 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Login from './pages/Login';
+// import NoMatch from './pages/NoMatch';
+// import Profile from './pages/Profile';
+// import Signup from './pages/Signup';
+
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:3001/graphql',
@@ -16,13 +22,20 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
-        <Header />
-        <div className='container'>
-          <Home />
+      <Router>
+        <div className='flex-column justify-flex-start min-100-vh'>
+          <Header />
+          
+          <Routes>
+            <Route
+                path="/" 
+                element={<Home />} 
+            />
+          </Routes>
+
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
