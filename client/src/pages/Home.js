f71671bca4e90055bf2145d7efae1504814326c5
crocket77 <ethanpfeiffer345@outlook.com>
2022-05-25 19:25:48 -0500

    added mentor board button on home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -6,10 +7,24 @@ const Home = () => {
       <div className='flex-row justify-space-between'>
         <div className='about col-12 mb-3 ml-3'>
           <h2 className='title2'>What is Life Sherpa:</h2>
-          <p className='w-100'>Life Sherpa is a place for you to find somebody to help you through whatever it is you need help with! We seek the best mentors from around the world that
-          can give you their life experience and help leapfrog you from where you are to where you want to be. Take a look at what our extraordinary list of mentors have to offer. 
-          From learning how to sing or play guitar to just having a dad or mom help you think about what you might be missing as you go through life.  </p>
+            <p className='w-100'>Life Sherpa is a place for you to find somebody to help you through whatever it is you need help with! We seek the best mentors from around the world that
+            can give you their life experience and help leapfrog you from where you are to where you want to be. Take a look at what our extraordinary list of mentors have to offer. 
+            From learning how to sing or play guitar to just having a dad or mom help you think about what you might be missing as you go through life.  
+            </p>
         </div>
+        <div>
+          <Link to={`/board/`}> 
+            <button className='button bg-secondary has-text-black-bis'>Check out our mentors</button>
+          </Link>
+        </div>
+        <div className='about col-12 mb-3 ml-3'>
+          <h5>Disclaimer:</h5>
+            <p className='w-100'>
+              Advice from mentors is to be taken at your own risk. 
+            </p>
+        </div>
+
+
       </div>
     </main>
   );
