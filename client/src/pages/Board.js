de47d59abf1a922ae9a8b3a92b1ecedb36ebd6ba
JCH95 <jhorger95@gmail.com>
2022-05-25 20:44:00 -0500

    added options for dropdown menu, added starting const functionality

--- client/src/pages/Board.js
+++ client/src/pages/Board.js
@@ -1,61 +1,80 @@
 import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import { useQuery } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
 
-const  Board= (props) => {
+const Board = (props) => {
+  const { username: userParam } = useParams();
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+    variables: { username: userParam },
+  });
+  const user = data?.me || data?.user || {};
 
-  // const { username: userParam } = useParams();
-
-  // const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-  //   variables: { username: userParam },
-  // });
-
-  // const user = data?.me || data?.user || {};
+  const [value, setValue] = React.useState('coding');
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  }
 
   // navigate to personal profile page if username is yours
-  // if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-  //   return <Navigate to="/profile:username" />;
-  // }
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Navigate to="/board" />;
+  }
 
-  // if (loading) {
-  //   return <div>Loading...</div>;
-  // }
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-  // if (!user?.username) {
-  //   return (
-  //     <h4>
-  //       You need to be logged in to see this. Use the navigation links above to
-  //       sign up or log in!
-  //     </h4>
-  //   );
-  // }
+  if (!user?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this. Use the navigation links above to
+        sign up or log in!
+      </h4>
+    );
+  }
   
   return (
-    <div>
-    <h1>Mentor Board</h1>
-
-
-      <div className="flex-row justify-space-between mb-3">
-        <div className="col-12 mb-3 col-lg-8">
-          {/* <ThoughtList
-            thoughts={user.thoughts}
-            title={`${user.username}'s thoughts...`}
-          /> */}
+    <main>
+      <h1>Mentor Board</h1>
+      <div className='flex-row justify-space-between'>
+        <div className='about col-12 mb-3 ml-3'>
+          <p className='w-100'>
+            Here you can find Mentor videos surrounding a plethora of topics! 
+          </p>
+          <div>
+            <label>
+              Mentor Video Topics
+              <select value={value} onChange={handleChange}>
+                <option value="coding">Coding</option>
+                <option value="fitness">Fitness</option>
+                <option value="music">Music</option>
+                <option value="nutrition">Nutrition</option>
+                <option value="gaming">Gaming</option>
+              </select>
+            </label>
+          </div>
+          <p className='w-100'>
+            Recently Added Videos:
+          </p>
+        </div>
+        <div>
+          <Link to={`/board/`}> 
+            <button className='button bg-secondary has-text-black-bis'>Search!</button>
+          </Link>
+        </div>
+        <div className='about col-12 mb-3 ml-3'>
+          <h5>Disclaimer:</h5>
+          <p className='w-100'>
+            Advice from mentors is to be taken at your own risk. 
+          </p>
         </div>
 
-        <div className="col-12 col-lg-3 mb-3">
-          {/* <FriendList
-            username={user.username}
-            friendCount={user.friendCount}
-            friends={user.friends}
-          /> */}
-        </div>
+
       </div>
-      {/* <div className="mb-3">{!userParam && <ThoughtForm />}</div> */}
-    </div>
+    </main>
   );
 };
 
