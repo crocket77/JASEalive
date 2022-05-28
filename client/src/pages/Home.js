2aec2522ac8d17899d7bc17eb0971085cf61be63
crocket77 <ethanpfeiffer345@outlook.com>
2022-05-28 12:59:31 -0500

    fixed merge conflict

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,16 +7,10 @@ const Home = () => {
   return (
     <main>
       <div className='flex-row justify-space-between'>
-<<<<<<< Updated upstream
-        <div className='about col-12 mb-3 ml-3 has-background-grey-lighter'>
-          <h2 className='title2'>What is Life Sherpa:</h2>
-            <p className='w-100'>Life Sherpa is a place for you to find somebody to help you through whatever it is you need help with! We seek the best mentors from around the world that
-=======
         <div id='box1'className=' container col-12 mb-3 ml-3 is-transparent  has-text-black-bis p-2 pl-9 pb-5'>
          <div  className='pl-9'>
            <h2 className='title2 has-text-black has-text-weight-medium'>What is Life Sherpa:</h2>
             <p className='w-100 has-text-black has-text-weight-medium'>Life Sherpa is a place for you to find somebody to help you through whatever it is you need help with! We seek the best mentors from around the world that
->>>>>>> Stashed changes
             can give you their life experience and help leapfrog you from where you are to where you want to be. Take a look at what our extraordinary list of mentors have to offer. 
             From learning how to sing or play guitar to just having a dad or mom help you think about what you might be missing as you go through life.  
             </p>
