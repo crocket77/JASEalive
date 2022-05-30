c086d0610f6c0cea05bd3f7978d2953cef91ef14
Andy McAlpin <AndrewMcAlpin@gmail.com>
2022-05-30 16:54:36 -0500

    basic wisdom list

---
+++ client/src/components/WisdomList/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const WisdomList = ({ wisdoms }) => {
+  if (!wisdoms.length) {
+    return <h3>No Wisdoms Yet</h3>;
+  }
+
+  return (
+    <div>
+      {/* <h3>{title}</h3> */}
+      {wisdoms &&
+        wisdoms.map(wisdom => (
+          <div key={wisdom._id} className="card mb-3">
+            <p className="card-header">
+              <Link
+                to={`/profile/${wisdom.username}`}
+                style={{ fontWeight: 700 }}
+                className="text-light"
+              >
+                {wisdom.username}
+              </Link>{' '}
+              {/* thought on {wisdom.createdAt} */}
+            </p>
+            <div className="card-body">
+                <p>{wisdom.thoughtText}</p>
+            </div>
+            <iframe width="560" height="315" src={`https://www.youtube.com/embed/${wisdom.youTubeLink}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default WisdomList;
