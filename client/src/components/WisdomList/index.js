897cfffbc865d9073b447d0f522e81939f91dc6b
JCH95 <jhorger95@gmail.com>
2022-05-31 20:21:57 -0500

    fixed css stuff for Board page

--- client/src/components/WisdomList/index.js
+++ client/src/components/WisdomList/index.js
@@ -57,7 +57,7 @@ const WisdomList = ({wisdoms, interest, username}) => {
       {/* <h3>{title}</h3> */}
       {wisdomArr &&
         wisdomArr.map(wisdom => (
-          <div key={wisdom._id} className="column mb-3 is-child box">
+          <div key={wisdom._id} className="column has-text-centered mb-3 mr-3 is-child box">
             <p className="card-header">
               <Link
                 to={`/profile/${wisdom.username}`}
@@ -68,7 +68,7 @@ const WisdomList = ({wisdoms, interest, username}) => {
               </Link>{' '}
               {/* thought on {wisdom.createdAt} */}
             </p>
-            <div className="column card-body">
+            <div className="column has-text-centered card-body">
               <p>{wisdom.wisdomText}</p>
             </div>
             {wisdom.youTubeLink &&
